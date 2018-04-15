8b0ca7cfcaa909a95a6009037da942e71bb6ce0b
jayz2053 <jpj19@students.uwf.edu>
2018-04-14 21:27:55 -0500

    seperation of routes

--- index.js
+++ index.js
@@ -5,28 +5,18 @@ var  morgan = require('morgan')
 var session = require('express-session')
 var mongoose = require('mongoose')
 var configDB = require('./.config/database.js')
-
-var opts = {
-  server:{
-    socketOptions:{keepAlive: 1}
-  }
-}
+var bodyParser = require('body-parser')
 
 
-mongoose.connect(configDB.url, opts)
+app.set('view engine', 'ejs')
+mongoose.connect(configDB.url, configDB.opts)
 
 
 app.use(morgan('dev'))
 app.use(session({secret: 'intro cookie',
                 saveUninitialized: true,
                 resave: true}))
-/*
-app.use('/', (req, res) => {
-  res.send('Hello World')
-  console.log('==============')
-  console.log(req.session)
-})
-*/
+
 require('./routes/routes1.js')(app)
 
 app.listen(port)
